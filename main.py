5439a69837dae83694d37e6562c188c89edb8fe6
Joseph Faiq <jfaiq94@gmail.com>
2025-08-16 21:13:26 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Enter a todo: ")
+# Store text into variable
+user_prompt = "Enter a todo: "
+# Ask user for input using predefined text
+text = input(user_prompt)
+# output user input
+print(text)
